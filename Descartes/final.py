fb40f0a57512f3e28c6ded0043cafb046474b33c
askartolegen <96340843+askartolegen@users.noreply.github.com>
2022-11-01 13:51:14 +0600

    Update final.py

--- Descartes/final.py
+++ Descartes/final.py
@@ -1,138 +1,7 @@
-import time
-
 import pygame
-import sqlite3
-from sys import exit
-from random import randint, choice
+from random import choice
 
-
-class Database:
-    __instance = None
-    cursor = None
-
-    def __new__(cls, *args, **kwargs):
-        if Database.__instance is None:
-            Database.__instance = super().__new__(cls)
-        return Database.__instance
-
-    def __del__(self):
-        Database.__instance = None
-
-    def __init__(self):
-        self.connection = sqlite3.connect("aquarium.db")
-        self.cursor = self.connection.cursor()
-        if Database.__instance is None:
-            self.cursor.execute("CREATE TABLE runner (name TEXT, score INTEGER)")
-
-    def add_player(self, name_, score_):
-        params = (name_, score_)
-        self.cursor.execute("INSERT INTO runner (name, score) VALUES (?, ?)", params)
-
-    def get_data(self):
-        rows = self.cursor.execute("SELECT name, score FROM runner order by score desc limit 5").fetchall()
-        return rows
-
-    def update_data(self, name_, score_):
-        params = (str(score_), name_)
-        self.cursor.execute("UPDATE runner SET score = ? WHERE name = ?", params)
-
-    def commit(self):
-        self.connection.commit()
-
-
-class Player(pygame.sprite.Sprite):
-    def __init__(self):
-        super().__init__()
-        player_walk_1 = pygame.image.load('graphics/Player/soldier_run_1.png').convert_alpha()
-        player_walk_1 = pygame.transform.rotozoom(player_walk_1, 0, 0.5)
-
-        player_walk_2 = pygame.image.load('graphics/Player/soldier_run_2.png').convert_alpha()
-        player_walk_2 = pygame.transform.rotozoom(player_walk_2, 0, 0.5)
-
-        player_walk_3 = pygame.image.load('graphics/Player/soldier_run_3.png').convert_alpha()
-        player_walk_3 = pygame.transform.rotozoom(player_walk_3, 0, 0.5)
-
-        player_walk_4 = pygame.image.load('graphics/Player/soldier_run_4.png').convert_alpha()
-        player_walk_4 = pygame.transform.rotozoom(player_walk_4, 0, 0.5)
-
-        player_walk_5 = pygame.image.load('graphics/Player/soldier_run_5.png').convert_alpha()
-        player_walk_5 = pygame.transform.rotozoom(player_walk_5, 0, 0.5)
-
-        player_walk_6 = pygame.image.load('graphics/Player/soldier_run_6.png').convert_alpha()
-        player_walk_6 = pygame.transform.rotozoom(player_walk_6, 0, 0.5)
-        self.player_walk = [player_walk_1, player_walk_2, player_walk_3, player_walk_4, player_walk_5, player_walk_6]
-        self.player_index = 0
-
-        player_jump = pygame.image.load('graphics/Player/soldier_jump.png').convert_alpha()
-        self.player_jump = pygame.transform.rotozoom(player_jump, 0, 0.65)
-
-        self.image = self.player_walk[self.player_index]
-        self.rect = self.image.get_rect(midbottom=(80, 300))
-        self.gravity = 0
-
-        self.jump_sound = pygame.mixer.Sound('audio/jump.mp3')
-        self.jump_sound.set_volume(0.5)
-
-    def player_input(self):
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_SPACE] and self.rect.bottom >= 300:
-            self.gravity = -20
-            self.jump_sound.play()
-
-    def apply_gravity(self):
-        self.gravity += 1
-        self.rect.y += self.gravity
-        if self.rect.bottom >= 300:
-            self.rect.bottom = 300
-
-    def animation_state(self):
-        if self.rect.bottom < 300:
-            self.image = self.player_jump
-        else:
-            self.player_index += 0.1
-            if self.player_index >= len(self.player_walk):
-                self.player_index = 0
-            self.image = self.player_walk[int(self.player_index)]
-
-    def update(self):
-        self.player_input()
-        self.apply_gravity()
-        self.animation_state()
-
-
-class Obstacle(pygame.sprite.Sprite):
-    def __init__(self, obstacle_type):
-        super().__init__()
-
-        if obstacle_type == 'rocket':
-            rocket = pygame.image.load('graphics/rocket.png').convert_alpha()
-            rocket = pygame.transform.scale(rocket, (72, 36))
-            self.frames = [rocket]
-            y_pos = 180
-        else:
-            mine = pygame.image.load('graphics/mine.png').convert_alpha()
-            mine = pygame.transform.scale(mine, (54, 18))
-            self.frames = [mine]
-            y_pos = 300
-
-        self.animation_index = 0
-        self.image = self.frames[self.animation_index]
-        self.rect = self.image.get_rect(midbottom=(randint(900, 1100), y_pos))
-
-    def animation_state(self):
-        self.animation_index += 0.1
-        if self.animation_index >= len(self.frames):
-            self.animation_index = 0
-        self.image = self.frames[int(self.animation_index)]
-
-    def update(self):
-        self.animation_state()
-        self.rect.x -= 6
-        self.destroy()
-
-    def destroy(self):
-        if self.rect.x <= -100:
-            self.kill()
+from objects import Player, Database, Obstacle
 
 
 class InputBox:
@@ -168,12 +37,12 @@ class InputBox:
 
     def update(self):
         # Resize the box if the text is too long.
-        width = max(200, self.txt_surface.get_width()+10)
+        width = max(200, self.txt_surface.get_width() + 10)
         self.rect.w = width
 
     def draw(self, screen_):
         # Blit the text.
-        screen_.blit(self.txt_surface, (self.rect.x+5, self.rect.y+5))
+        screen_.blit(self.txt_surface, (self.rect.x + 5, self.rect.y + 5))
         # Blit the rect.
         pygame.draw.rect(screen_, self.color, self.rect, 2)
 
@@ -200,7 +69,7 @@ def collision_sprite():
 
 def display_text(data):
     tb = data[:]
-    x, y = 580, 60
+    x, y = 580, 30
     word_surface = test_font.render('Top 5 players:', True, (64, 64, 64))
     screen.blit(word_surface, (x, y))
 
@@ -213,12 +82,20 @@ def display_text(data):
         screen.blit(word_surface, (x, y))
 
 
+def reset():
+    global score, player_alive
+    score = 0
+    player_alive = True
+
+
 pygame.init()
 screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
 test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
-game_active = False
+player_alive = False
+start_page = True
+mouse_pos = (-1, -1)
 start_time = 0
 score = 0
 bg_music = pygame.mixer.Sound('audio/music.wav')
@@ -236,16 +113,32 @@ sky_surface = pygame.image.load('graphics/Sky.png').convert_alpha()
 sky_surface = pygame.transform.scale(sky_surface, (800, 300))
 ground_surface = pygame.image.load('graphics/ground.png').convert_alpha()
 
-# Intro screen
+# Texts
 player_stand = pygame.image.load('graphics/Player/soldier_stand.png').convert_alpha()
-player_stand = pygame.transform.rotozoom(player_stand, 0, 1)
-player_stand_rect = player_stand.get_rect(center=(300, 200))
+player_stand = pygame.transform.rotozoom(player_stand, 0, 0.55)
+player_stand_rect = player_stand.get_rect(midbottom=(100, 300))
 
-game_name = test_font.render('Pixel Runner', False, (111, 196, 169))
-game_name_rect = game_name.get_rect(center=(300, 80))
+game_name = test_font.render('Pixel Runner', False, (64, 64, 64))
+game_name_rect = game_name.get_rect(center=(350, 50))
 
-game_message = test_font.render('Enter name and press space to run', False, (111, 196, 169))
-game_message_rect = game_message.get_rect(center=(300, 330))
+game_over_img = pygame.image.load('graphics/game_over.png')
+game_over_img = pygame.transform.rotozoom(game_over_img, 0, 1)
+game_over_img_rect = game_over_img.get_rect(center=(350, 200))
+
+replay_img = pygame.image.load('graphics/replay.png')
+replay_img = pygame.transform.rotozoom(replay_img, 0, 1)
+replay_img_rect = replay_img.get_rect(center=(350, 250))
+
+game_message1 = test_font.render('Enter name', False, (64, 64, 64))
+game_message1_rect = game_message1.get_rect(center=(350, 150))
+game_message2 = test_font.render('and press space to run', False, (64, 64, 64))
+game_message2_rect = game_message2.get_rect(center=(350, 200))
+
+win_message = test_font.render("You WIN", False, 'Gold')
+win_message_rect = win_message.get_rect(center=(400, 200))
+
+input_message = test_font.render("Enter a name:", False, (64, 64, 64))
+input_message_rect = input_message.get_rect(center=(240, 370))
 
 # Timer
 obstacle_timer = pygame.USEREVENT + 1
@@ -256,62 +149,81 @@ COLOR_INACTIVE = pygame.Color('Grey')
 COLOR_ACTIVE = pygame.Color('White')
 input_box = InputBox(350, 350, 100, 32)
 
-while True:
+running = True
+while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             db.commit()
-            pygame.quit()
-            exit()
+            running = False
 
-        if game_active:
+        if player_alive:
             if event.type == obstacle_timer:
                 obstacle_group.add(Obstacle(choice(['rocket', 'mine', 'mine', 'mine'])))
 
         else:
-            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-                game_active = True
-                start_time = int(pygame.time.get_ticks() / 1000)
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE or event.key == pygame.K_q:
+                    running = False
+
+                if event.key == pygame.K_SPACE:
+                    if start_page:
+                        start_page = False
+                        player_alive = True
+                        start_time = int(pygame.time.get_ticks() / 1000)
             input_box.handle_event(event)
 
-    if game_active:
-        screen.blit(sky_surface, (0, 0))
-        screen.blit(ground_surface, (0, 300))
-        score = display_score()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_pos = event.pos
 
-        player.draw(screen)
-        player.draw(screen)
-        player.update()
+        if event.type == pygame.MOUSEBUTTONUP:
+            mouse_pos = (-1, -1)
 
-        obstacle_group.draw(screen)
-        obstacle_group.update()
-        name = input_box.get_text()
-        game_active = collision_sprite()
+    screen.blit(sky_surface, (0, 0))
+    screen.blit(ground_surface, (0, 300))
 
-        if score >= 100:
-            win_message = test_font.render("You WIN", False, 'Gold')
-            win_message_rect = win_message.get_rect(center=(400, 200))
-            screen.blit(win_message, win_message_rect)
-
-    else:
-        screen.fill((94, 129, 162))
+    if start_page:
         screen.blit(player_stand, player_stand_rect)
-
-        score_message = test_font.render(f'Your score: {score}', False, (111, 196, 169))
-        score_message_rect = score_message.get_rect(center=(300, 330))
         screen.blit(game_name, game_name_rect)
         display_text(db.get_data())
-
-        input_message = test_font.render("Enter a name:", False, (111, 196, 169))
-        input_message_rect = input_message.get_rect(center=(240, 370))
         screen.blit(input_message, input_message_rect)
-
         input_box.update()
         input_box.draw(screen)
+        screen.blit(game_message1, game_message1_rect)
+        screen.blit(game_message2, game_message2_rect)
 
-        if score == 0:
-            screen.blit(game_message, game_message_rect)
+    else:
+        if player_alive:
+            score = display_score()
+            player.draw(screen)
+            player.draw(screen)
+            player.update()
+
+            obstacle_group.draw(screen)
+            obstacle_group.update()
+            name = input_box.get_text()
+            player_alive = collision_sprite()
+
+            if score >= 100:
+                screen.blit(win_message, win_message_rect)
+
         else:
+            display_text(db.get_data())
+            screen.blit(game_over_img, game_over_img_rect)
+            screen.blit(replay_img, replay_img_rect)
+            screen.blit(player_stand, player_stand_rect)
+
+            score_message = test_font.render(f'Your score: {score}', False, (64, 64, 64))
+            score_message_rect = score_message.get_rect(center=(350, 50))
+
             screen.blit(score_message, score_message_rect)
+            screen.blit(input_message, input_message_rect)
+            input_box.update()
+            input_box.draw(screen)
+            if replay_img_rect.collidepoint(mouse_pos):
+                start_time = int(pygame.time.get_ticks() / 1000)
+                player_alive = True
 
     pygame.display.update()
     clock.tick(60)
+
+pygame.quit()
